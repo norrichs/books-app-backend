50a6ab7d221e90045b046b625a3141a946c86d4a
Ben Norrichs <ben.norrichs@gmail.com>
2021-05-18 16:13:59 -0400

    backend functional

--- controllers/book.js
+++ controllers/book.js
@@ -3,8 +3,9 @@ const { Router } = require("express");
 const router = Router();
 
 // get Seed Data
-const seedData = require('../db/seedData.json')
-console.log(seedData)
+const bookSeed = require('../db/seedData.json')
+// console.log(bookSeed)
+
 //index route
 router.get("/", async (req, res) => {
   res.json(await Book.find({}));
@@ -17,6 +18,7 @@ router.post("/", async (req, res) => {
 
 //update route
 router.put("/:id", async (req, res) => {
+  console.log(req.params.id, req.body)
   res.json(await Book.findByIdAndUpdate(req.params.id, req.body, { new: true }));
 });
 
@@ -24,6 +26,25 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   res.json(await Book.findByIdAndRemove(req.params.id));
 });
+
+
+// Seed Route for Seeding Database
+router.get("/seed", async (req, res) => {
+	// try block for catching errors
+	try {
+	  // remove all books from database
+	  await Book.remove({});
+	  // add the seed data to the database
+	  await Book.create(bookSeed);
+	  // get full list of books to confirm seeding worked
+	  const books = await Book.find({});
+	  // return full list of books as JSON
+	  res.json(books);
+	} catch (error) {
+	  // return error as JSON with an error status
+	  res.status(400).json(error);
+	}
+  });
 
 // EXPORT ROUTER
 module.exports = router;
